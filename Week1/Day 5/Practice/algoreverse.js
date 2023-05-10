59bcb2b5c607897dc7caed0d6f598409915abde2
Dhia-Abdelmoula <131872606+Dhia-Abdelmoula@users.noreply.github.com>
2023-05-10 16:45:12 +0200

    profile page

---
+++ Week1/Day 5/Practice/algoreverse.js
@@ -0,0 +1,17 @@
+
+var j = 0;
+var temp='';
+var arr =['b','c','d','e','a'];
+for (let i = 0;i<=arr.length/2;i++){
+    j=arr.length-i-1;
+    temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+    
+}
+console.log(arr);
+var b ;
+console.log (6/3);
+var arr=[arr,arr]
+console.log(arr)
+
---
+++ Week1/Day 5/Practice/random 1to6.js
@@ -0,0 +1,4 @@
+function random1to6 (){
+    return(Math.ceil(Math.random()*6))
+}
+console.log(random1to6());
